b14372a3067c5e129b03ae0f140215275bbb4d34
Rivene <82700044+Rivene@users.noreply.github.com>
2024-02-02 10:47:43 +0900

    [level 0] Title: 배열의 평균값, Time: 0.16 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public double solution(int[] numbers) {
+        double sum = 0;
+        for(int i=0;i<numbers.Length;i++)
+        {
+            sum += numbers[i];
+        }
+        sum /= numbers.Length;
+        return sum;
+    }
+}
