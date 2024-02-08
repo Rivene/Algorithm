f671864ccc0698672978837c7931ca3da21a146c
Rivene <82700044+Rivene@users.noreply.github.com>
2024-02-08 09:16:41 +0900

    [level 1] Title: 하샤드 수, Time: 0.18 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool solution(int x) {
+        bool answer = false;
+        
+        int num = x;
+        int sum = 0;
+        while(num / 10 != 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+        sum += num;
+        
+        if(x % sum == 0)
+            answer = true;
+        
+        return answer;
+    }
+}
