01f76000618fe6dc2175db845bb4286ab45fbd7a
Rivene <82700044+Rivene@users.noreply.github.com>
2024-02-23 15:09:59 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.34 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cs
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cs
@@ -3,19 +3,16 @@ public class Solution {
         string answer = "";
         char[] charArray = new char[s.Length];
         
-        for(int i = 0; i < s.Length; i++){
+        for(int i = 0; i < s.Length; i++)
             charArray[i] = s[i];
-        }
         
-        for(int i = 0; i < s.Length; i++){
-            for(int j = 0; j < s.Length - 1; j++){
+        for(int i = 0; i < s.Length-1; i++)
+            for(int j = 0; j < s.Length - 1; j++)
                 if((int)charArray[j] < (int)charArray[j+1]){
                     char temp = charArray[j];
                     charArray[j] = charArray[j + 1];
                     charArray[j + 1] = temp;
                 }
-            }
-        }
         
         for(int i = 0; i < s.Length; i++)
             answer += charArray[i];
