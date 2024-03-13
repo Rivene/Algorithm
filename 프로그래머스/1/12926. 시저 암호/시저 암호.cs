01ada93acdf242c90859b1d8d4161149851a540d
Rivene <82700044+Rivene@users.noreply.github.com>
2024-03-13 18:04:34 +0900

    [level 1] Title: 시저 암호, Time: 34.76 ms, Memory: 47.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public string solution(string s, int n) {
+        string answer = "";
+        
+        for(int i = 0; i < s.Length; i++){
+            if(s[i] != ' '){
+                if((s[i] + n > 90 && s[i] < 91) || s[i] + n > 122)
+                    answer += (char)(s[i] + n - 26);      
+                else
+                    answer += (char)(s[i] + n);
+            }
+            else
+                answer += ' ';
+        }
+        
+        return answer;
+    }
+}
