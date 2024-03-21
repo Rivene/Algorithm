3b497be8218cb38e27b728acc90112030c58281a
Rivene <82700044+Rivene@users.noreply.github.com>
2024-03-21 12:49:36 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.41 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cs
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cs
@@ -1,16 +1,14 @@
-using System;
 public class Solution {
     public string[] solution(string[] strings, int n) {
-        int len = strings.Length;
-        string[] answer = new string[len];
+        string[] answer = new string[strings.Length];
         
         //각 문자열의 n번째 글자를 앞으로 더한 문자열을 저장
-        for(int i = 0; i < len; i++)
+        for(int i = 0; i < strings.Length; i++)
             answer[i] = (strings[i][n]) + strings[i];
         
         //오름차순 정렬
-        for(int i = 0; i < len; i++){
-            for(int j = i + 1; j < len; j++){
+        for(int i = 0; i < strings.Length; i++){
+            for(int j = i + 1; j < strings.Length; j++){
                 for(int k = 0; k < answer[0].Length; k++){
                     if(answer[i][k] > answer[j][k]){
                         string temp = answer[i];
@@ -26,8 +24,8 @@ public class Solution {
         }
         
         //정렬된 문자열에 더했던 n번째 글자를 Substring(1)으로 제거
-        for(int j = 0; j < len; j++)
-            answer[j] = answer[j].Substring(1);
+        for(int i = 0; i < strings.Length; i++)
+            answer[i] = answer[i].Substring(1);
         
         return answer;
     }
