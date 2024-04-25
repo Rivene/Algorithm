d895d457481d7d827435df907a3d5a88cf357f5f
Rivene <82700044+Rivene@users.noreply.github.com>
2024-04-25 20:45:37 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 115.39 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Solution {
+    public int[] solution(string s) {
+        int[] answer = new int[s.Length];
+        
+        int a;
+        answer[0] = -1;
+        
+        for(int i = 1; i < s.Length; i++){
+            for(int j = 0; j < i; j++){
+                if(s[i] == s[j])
+                    answer[i] = i - j;
+            }
+            if(answer[i] == 0)
+                answer[i] = -1;
+        }
+        
+        return answer;
+    }
+}
