7a5d8ce43c9a8a3b3129e04632a22da6edcaaff6
Rivene <82700044+Rivene@users.noreply.github.com>
2024-04-26 15:12:07 +0900

    [level 1] Title: 콜라 문제, Time: 0.18 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
@@ -3,7 +3,6 @@ using System;
 public class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        int rest = 0;
         
         while(n >= a){
             answer += n / a * b;
