79852acc309b5085c297a431709ced49c6cc8bda
Rivene <82700044+Rivene@users.noreply.github.com>
2024-04-26 09:45:28 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.52 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cs
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cs
@@ -4,17 +4,15 @@ public class Solution {
     public string solution(int[] food) {
         string answer = "";
         
-        for(int i = 1; i < food.Length; i++){
+        for(int i = 1; i < food.Length; i++)
             for(int j = 0; j < food[i] / 2; j++)
-            answer += i.ToString();
-        }
+                answer += i.ToString();
         
         answer += '0';
         
-        for(int i = food.Length - 1; i > 0; i--){
+        for(int i = food.Length - 1; i > 0; i--)
             for(int j = 0; j < food[i] / 2; j++)
-            answer += i.ToString();
-        }
+                answer += i.ToString();
         
         return answer;
     }
