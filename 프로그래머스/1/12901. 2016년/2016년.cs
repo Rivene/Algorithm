dbd997f8d3076ac8ef195ca243e4d3f79f6ffdf3
Rivene <82700044+Rivene@users.noreply.github.com>
2024-05-22 20:54:33 +0900

    [level 1] Title: 2016년, Time: 2.20 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cs
+++ 프로그래머스/1/12901. 2016년/2016년.cs
@@ -3,7 +3,9 @@ using System;
 public class Solution {
     public string solution(int a, int b) {
         DateTime date = new DateTime(2016, a, b);
+        
         string dayOfWeek = date.DayOfWeek.ToString().ToUpper().Substring(0, 3);
+        
         return dayOfWeek;
     }
 }
