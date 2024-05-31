6f0760301351ef031452cfa18df2a83058574546
Rivene <82700044+Rivene@users.noreply.github.com>
2024-05-31 15:16:23 +0900

    [level 1] Title: 과일 장수, Time: 2.48 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.cs
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cs
@@ -3,15 +3,18 @@ using System.Linq;
 
 public class Solution {
     public int solution(int k, int m, int[] score) {
-        // 점수를 내림차순으로 정렬합니다.
+        // score를 내림차순으로 정렬
         Array.Sort(score);
         Array.Reverse(score);
         
+        // return 시킬 최대 이익 값 초기화
         int answer = 0;
+        
+        // 상자 생성 후 큰 수부터 입력하여 이익 계산
         for(int i = 0; i < score.Length / m; i++){
             int[] box = new int[m];
             for(int j = 0; j < m; j++){
-                box[j] += score[j + i * m];
+                box[j] += score[i * m + j];
             }
             answer += box[m - 1] * m;
         }
