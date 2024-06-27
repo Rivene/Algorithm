6d562b75767e8fed724b88dd5344d3c44fac0b3e
Rivene <82700044+Rivene@users.noreply.github.com>
2024-06-27 17:40:15 +0900

    [level 1] Title: 소수 만들기, Time: 0.21 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.cs
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cs
@@ -4,29 +4,20 @@ class Solution
 {
     public int solution(int[] nums)
     {
-        int answer = 0;
-        int complex;
+        int answer = 0;  // 리턴할 값 : 소수되는 경우
+        int complex = 0; // 합성수인 경우 증가
         
-        for(int i = 0; i < nums.Length; i++){
-            for(int j = i + 1; j < nums.Length; j++){
+        for(int i = 0; i < nums.Length; i++)
+            for(int j = i + 1; j < nums.Length; j++)
                 for(int k = j + 1; k < nums.Length; k++){
                     complex = 0;
-                    for(int cases = nums[i] + nums[j] + nums[k] - 1; cases > 1; cases--){
-                        if((nums[i] + nums[j] + nums[k]) % cases == 0){
+                    for(int cases = nums[i] + nums[j] + nums[k] - 1; cases > 1; cases--)
+                        if((nums[i] + nums[j] + nums[k]) % cases == 0)
                             complex++;
-                            break;
-                        }
-                    }
                     if(complex == 0)
                         answer++;
                 }
-            }
-        }
         
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        //Console.Write(complex);
-
         return answer;
     }
 }
