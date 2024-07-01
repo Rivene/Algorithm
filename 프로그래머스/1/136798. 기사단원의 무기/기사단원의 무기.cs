02c670c4cd0274b28752fc1be2c86ff1f2c89da7
Rivene <82700044+Rivene@users.noreply.github.com>
2024-07-01 18:16:00 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.43 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cs
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cs
@@ -7,6 +7,8 @@ public class Solution {
         
         for(int i = 1; i <= number; i++){
             count = 0;
+            
+            // 모든 약수 검색 시 시간초과로 인해 제곱근 전까지만 구하고 두 배로 계산
             for(int num = 1; num * num <= i; num++){
                 if(i % num == 0){
                     if(i / num == num)
