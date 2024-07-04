0c2bbaa0e7ed7706979b9a8174591b2ac8a2740d
Rivene <82700044+Rivene@users.noreply.github.com>
2024-07-04 16:34:48 +0900

    [level 1] Title: 숫자 짝꿍, Time: 2.63 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cs
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cs
@@ -4,20 +4,29 @@ using System.Linq;
 
 public class Solution {
     public string solution(string X, string Y) {
-        var countX = new int[10];
-        var countY = new int[10];
+        /// 1. 문자열 X, Y에 각 숫자가 몇 번 등장하는지 저장할 정수 배열 생성
+        int[] countX = new int[10];
+        int[] countY = new int[10];
         
-        foreach (var c in X) countX[c - '0']++;
-        foreach (var c in Y) countY[c - '0']++;
+        /// 2. 문자열 X, Y의 각 문자에 대해 해당하는 숫자 등장 횟수 증가
+        foreach (char c in X)
+            countX[Int32.Parse(c.ToString())]++; // 해당 문자를 문자열로 변경 후 정수로 변경하는 방식
+        foreach (char c in Y)
+            countY[c - '0']++; // 해당 문자와 '0'의 아스키 코드 값의 차이를 이용하는 방식
         
-        var result = new List<int>();
-        for (int i = 9; i >= 0; i--) {//[^1^][1]
+        /// 3. 9부터 0까지의 숫자에 대해 해당 숫자가 X, Y에 동시 등장 횟수를 계산하고 result 리스트에 추가
+        List<int> result = new List<int>();
+        for (int i = 9; i >= 0; i--){
             result.AddRange(Enumerable.Repeat(i, Math.Min(countX[i], countY[i])));
         }
         
-        if (result.Count == 0) return "-1";
-        if (result.All(d => d == 0)) return "0";
+        /// 4-1. result 리스트가 비어있으면 -1을, 모든 요소가 0이면 0을 반환
+        if (result.Count == 0)
+            return "-1";
+        if (result.All(d => d == 0))
+            return "0";
         
+        /// 4-2. result 리스트의 모든 요소를 문자열로 변환하여 연결 후 반환
         return string.Join("", result);
     }
 }
